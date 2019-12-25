8ab83e73c76cb3b00c7bb024a92c6c2d1da8f651
bq-bq <1595112703@qq.com>
2019-12-25 16:29:06 +0800

    update

---
+++ AR/Assets/VirtualButtonEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+[System.Obsolete]
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vb;
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("press");
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("release");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            Debug.Log("this");
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
